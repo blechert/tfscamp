635a115b4851b662c7cc703bad6e0b38a915bda8
Maximilian Carl <maximilian.carl@cc-suc.de>
2019-12-05 16:13:50 +0100

    add ps script to retrieve workitem ids from PR

---
+++ Get-WorkItemIDs.ps1
@@ -0,0 +1,66 @@
+#Requires -version 4
+<#
+.SYNOPSIS
+    Retrive WorkItem data from PR-IDs
+.DESCRIPTION
+.NOTES
+.COMPONENT
+.EXAMPLE
+.\Get-WorkItemIDs.ps1 -Uri 'https://dev.azure.com/{organisation}}/{project}' -PAT '<YourToken>' -PRIDs @(1,2,3)
+#>
+[CmdletBinding()]
+[OutputType([string])]
+param(
+    # Array of WorkItem IDs
+    [Parameter(Mandatory=$true, ValueFromPipeline)]
+        [int[]] $PRIDs,
+    # https://dev.azure.com/{organization}/{project}
+    [Parameter(Mandatory=$true)]
+        [string] $Uri,
+    # Access token
+    [Parameter(Mandatory=$true)]
+        [string] $PAT
+)
+
+Begin {
+    function Get-WIIDs {
+        [OutputType([PSCustomObject])]
+        param (
+            # Azure DevOps Rest Api Call
+            [Parameter(Mandatory=$true)]
+                [string] $Uri,
+            # Access Token
+            [Parameter(Mandatory=$true)]
+                [string] $PAT
+        )
+
+        Begin {
+            if($PAT -ne '') {
+                $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f '',$PAT)))
+            } else {
+                Write-Error "WTF: Wo ist der PAT"
+            }
+        }
+
+        Process {
+            Write-Verbose ("Connect to: {0}" -f $Uri)
+            return Invoke-WebRequest -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Uri $Uri -ContentType 'application/json' -Method Get | ConvertFrom-Json
+        }
+
+        End {}
+    }  
+}
+
+Process {
+    foreach($PRID in $PRIDs) {
+        $request_uri = ("{0}/_apis/git/repositories/{1}/pullRequests/{2}/workitems?api-version={3}" -f $Uri, $env:BUILD_REPOSITORY_NAME, $PRID, '5.1')
+        $res = Get-WIIDs -Uri $request_uri -PAT $PAT
+
+        foreach ($value in $res.value)  
+        {
+            Write-Host ($value.'id')
+        }        
+    }
+}
+
+End {}
